a03b23c2b0a058a1e19c3f82e59d7e4ec6634c85
benyo <benyo.mt@gmail.com>
2023-06-15 23:00:57 -0400

    1 file modified

--- src/components/ShowListTapePanel.tsx
+++ src/components/ShowListTapePanel.tsx
@@ -1,5 +1,6 @@
 import * as R from 'ramda';
-export function ShowListTapePanel({ title, colorTitle, listItems }) {
+import { abbreviateNumber } from '../modules/GetListPriceWithQtyBiggerApp';
+export function ShowListTapePanel({ title, colorTitle, listItems }: any) {
   return (
     <div className="bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:rounded-lg">
       <h1 className={`uppercase font-bold text-center ${colorTitle}`}>
@@ -24,7 +25,7 @@ export function ShowListTapePanel({ title, colorTitle, listItems }) {
                   {item.price}
                 </td>
                 <td className="py-4 px-6 border-b border-gray-300">
-                  {R.last(item.quantities)}
+                  {abbreviateNumber(Number(R.last(item.quantities)))}
                 </td>
               </tr>
             );
